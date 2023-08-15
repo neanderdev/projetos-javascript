bc8a7ccdb885508f7d37de17da2dc77ef8b88915
Neander de Souza <neander.faria@hotmail.com>
2023-08-15 10:54:20 -0300

    Manipulando cores hexadecimais com JavaScript puro

---
+++ manipulando-cores-hexadecimais-js/main.js
@@ -0,0 +1,55 @@
+const { body } = document;
+
+try {
+  body.style.backgroundColor = lumiance("#6633cc", -0.6);
+} catch (e) {
+  console.log("Houve um erro: " + e.message);
+}
+
+// lógica para converter o hex em uma cor mais clara
+// ou mais escura
+function lumiance(hex, luminosity = 0) {
+  // hexadecimal é um valor que vai de 0 até f
+  // contém 16 dígitos
+  // 0 = black
+  // f = white
+
+  // eu aceito hex decimal com 3 ou 6 dígitos
+  hex = hex.replace(/[^0-9a-f]/gi, "");
+
+  const isValidHex = hex.length === 6 || hex.length === 3;
+  if (!isValidHex) throw new Error("Invalid HEX");
+
+  // se for 3 dígitos, transformar para 6 dígitos
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+
+  // aplicar uma fórmula matemática para aumentar ou
+  // diminuir a luminosidade de um hex
+
+  // precisa transformar o hex em rgb
+  // r (red) / g (green) / b (blue)
+  const black = 0;
+  const white = 255;
+
+  const twoDigitGroup = hex.match(/([0-9a-f]){2}/gi);
+
+  let newHex = "#";
+
+  for (let twoDigit of twoDigitGroup) {
+    const numberFromHex = parseInt(twoDigit, 16);
+    const calculateLuminosity = numberFromHex + luminosity * 255;
+    const blackOrLuminosity = Math.max(black, calculateLuminosity);
+    const partialColor = Math.min(white, blackOrLuminosity);
+
+    const newColor = Math.round(partialColor);
+
+    const numberToHex = newColor.toString(16);
+    const finalNumber = `0${numberToHex}`.slice(-2);
+
+    newHex = newHex + finalNumber;
+  }
+
+  return newHex;
+}
